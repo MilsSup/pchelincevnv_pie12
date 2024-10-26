1515ee3039bbd07672916c7d68e6df8390cf1b39
MilsSup <lireagain@gmail.com>
2024-10-26 08:35:15 +0700

    rainbow starrain
    дождь из разноцветных звезд

--- app.js
+++ app.js
@@ -1,26 +1,20 @@
-const nameInput = document.getElementById('nameInput')
-const greetBtn = document.getElementById('greetBtn')
-const personalGreeting = document.getElementById('personalGreeting')
-const changeColorBtn = document.getElementById('changeColorBtn')
+let quantity = window.innerWidth > 768 ? 200 : 100 // Больше капель на десктопе, меньше на мобильных
+let body = document.querySelector('body')
+let index = 0
 
-greetBtn.addEventListener('click', () => {
-	const name = nameInput.value.trim()
-	if (name) {
-		personalGreeting.textContent = `Привет, ${name}!`
-	} else {
-		personalGreeting.textContent = 'Привет, гость!'
-	}
-})
+while (index < quantity) {
+	let drop = document.createElement('div')
+	drop.classList.add('index')
+	let size = Math.random() * 5
+	let positionX = Math.floor(Math.random() * window.innerWidth)
+	let delay = Math.random() * -20
+	let duration = Math.random() * 5
 
-const colors = [
-	'lightblue',
-	'lightgreen',
-	'lightcoral',
-	'lightgoldenrodyellow',
-	'lightpink',
-]
+	drop.style.width = 0.2 + size + 'px'
+	drop.style.left = positionX + 'px'
+	drop.style.animationDelay = delay + 's'
+	drop.style.animationDuration = 7 + duration + 's'
 
-changeColorBtn.addEventListener('click', () => {
-	const randomColor = colors[Math.floor(Math.random() * colors.length)]
-	document.body.style.backgroundColor = randomColor
-})
+	body.appendChild(drop)
+	index++
+}
